fcd5f7600b21891fd8902c13f28334d86950e57e
Mathieu Lecarme <mathieu@garambrogne.net>
2012-05-27 20:53:41 +0200

    autoscale and axis.

--- www/metrics.js
+++ www/metrics.js
@@ -5,17 +5,6 @@ function escape(e) {
     return e.replace(/[#.]/g, '_');
 }
 var remembers = {};
-// X scale will fit values from 0-10 within pixels 0-100
-var x = d3.scale.linear().domain([0, 100]).range([0, 50]);
-// Y scale will fit values from 0-10 within pixels 0-100
-var y = d3.scale.linear().domain([0, 100]).range([30, 0]);
-var draw = d3.svg.line().
-x(function(d, i) {
-    console.log('d i', d, i);
-    return x(i) * 10;}).
-y(function(d) {
-    console.log(d);
-    return y(d[1]);});
 /**
  * Handle a new message
  * @param {event} event An event.
@@ -29,19 +18,36 @@ stocks.onmessage = function(event) {
         remembers[k].set(data[k]);
         console.log('limits', remembers[k].limits());
         console.log('items', remembers[k].items());
+        // X scale will fit values from 0-10 within pixels 0-100
+        var x = d3.scale.linear().domain([0, 100]).range([0, 50]);
+        // Y scale will fit values from 0-10 within pixels 0-100
+        var y = d3.scale.linear().
+            domain([0, remembers[k].limits().value.max]).
+            range([25, 0]);
+        var draw = d3.svg.line().
+            x(function(d, i) {
+                console.log('d i', d, i);
+                return x(i) * 5;}).
+            y(function(d) {
+                console.log(d);
+                return y(d[1]);});
         var line = table.select('#' + escape(k));
+        var axis = d3.svg.axis().scale(x).tickSubdivide(3);
         if (! line.empty()) { // the line exists
             line.text(Math.round(data[k]));
         } else {
-            table.append('div').attr('class', 'data').html('<h2 class="title">' + k +
+            table.append('div').attr('class', 'data').
+                html('<h2 class="title">' + k +
                     '</h2><div class="value" id="' + escape(k) + '">' +
                     Math.round(data[k]) + ' </div>' +
                     '<div class="graph" ><svg class="draw">' +
+                    '<g/>' +
                     '<path class="line" id="_' +
                     escape(k) + '"/></svg>');
         }
-        var l = d3.select('#_' + escape(k)).
+        d3.select('#_' + escape(k)).
             attr('d', draw(remembers[k].items()));
+        d3.select('#' + escape(k) + ' g').call(axis);
 
     }
 };
