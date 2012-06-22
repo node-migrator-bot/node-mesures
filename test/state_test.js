61bfd0563432b78e50de5e229304f7def87d5d29
Mathieu Lecarme <mlecarme@bearstech.com>
2012-06-22 16:11:18 +0200

    State can now incr and flush.

--- test/state_test.js
+++ test/state_test.js
@@ -7,4 +7,13 @@ describe('State', function() {
         s.set('popo', 42);
         s.get('popo').should.equal(42);
     });
+    it('should set a value after some time', function(done) {
+        var s = new state.State();
+        s.on('set', function(key, value) {
+            s.get('popo').should.equal(3);
+            done();
+        });
+        s.incr('popo', 1, 10);
+        s.incr('popo', 2);
+    });
 });
