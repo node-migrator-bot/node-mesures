25fefe87dde37629d7e4cd5dd803578fcac77ec9
Mathieu Lecarme <mathieu@garambrogne.net>
2012-06-26 23:25:40 +0200

    get some stuffs.

--- test/cube_test.js
+++ test/cube_test.js
@@ -1,9 +1,13 @@
-var cube = require('../lib/output/cube');
+var cube = require('../lib/output/cube'),
+    hoard = require('hoard'),
+    path = require('path'),
+    fs = require('fs');
 require('should');
 
 var mockState = { on: function(evt, cb) {}};
 var mockRouter = { route: function(path, cb) {}};
 
+//[FIXME] clean hoard tables before test
 describe('Cube', function() {
     it('should accept value', function(done) {
         var c = new cube.Cube(mockState, mockRouter);
@@ -25,4 +29,44 @@ describe('Cube', function() {
         r.should.have.lengthOf(2);
         r.should.eql(['pim.pam.poum', 'pim.captain.poum']);
     });
+    it('should get some values', function(done) {
+        var DB = '/tmp/get_test';
+        var c = new cube.Cube(mockState, mockRouter);
+        feed(DB, 50, function() {
+            var now = Math.floor(Date.now() / 1000);
+            c.get('get_test MEAN', now - 50 , now, 5, function(err, values) {
+                if (err) throw err;
+                console.log('values', values);
+                done();
+            });
+
+
+        });
+    });
 });
+
+/*
+ * Create a random hoard db.
+ */
+function feed(db, size, cb) {
+    if (path.existsSync(db)) {
+        fs.unlinkSync(db);
+    }
+    hoard.create(db, [[1, 100], [10, 6000]], 0.5, function(err) {
+        if (err) throw err;
+        var now = Math.floor(Date.now() / 1000);
+        var cpt = 50;
+        //[FIXME] use hoard multiple assignement.
+        var update = function() {
+            cpt--;
+            if (cpt == 0) {
+                cb.call();
+            } else {
+                hoard.update(db, Math.floor(100 * Math.random()), now - cpt,
+                    update);
+            }
+        }
+        update();
+    });
+
+}
